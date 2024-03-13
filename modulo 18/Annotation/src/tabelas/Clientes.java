de83f71cfbe1fc0b8acb20d5e59891a0bc7dfbad
Caioliu <caioarthur_@hotmail.com>
2024-03-13 14:42:47 -0300

    upload exercicio m18

---
+++ modulo 18/Annotation/src/tabelas/Clientes.java
@@ -0,0 +1,11 @@
+package tabelas;
+
+import annotation.TabelaAnnotation;
+
+@TabelaAnnotation("Clientes")
+public class Clientes {
+    private int id;
+    private String nome;
+    private int idade;
+
+}
---
+++ modulo 18/Annotation/src/tabelas/Vendas.java
@@ -0,0 +1,12 @@
+package tabelas;
+
+import annotation.TabelaAnnotation;
+
+@TabelaAnnotation("Vendas")
+public class Vendas {
+    private int id;
+    private String cliente;
+    private double valorTotal;
+    private String data;
+
+}
